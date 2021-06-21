6f7afb79fc6cbe58cc912f7769e53193ad84a1f0
marquisalexander <marquissantos123@gmail.com>
2021-06-21 20:32:50 -0300

    starting second day

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import { SignIn } from './src/screens/SignIn';
+import React from "react";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+
+import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
-  return (
-    <SignIn />
-  );
+  useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  return <SignIn />;
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,25 +1,36 @@
-import React, {useState} from 'react';
-import {View, Text, TextInput, Image, StatusBar} from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, Image, StatusBar } from "react-native";
 
-import { ButtonIcon } from '../../components/ButtonIcon';
-import IllustrationImg from '../../assets/illustration.png';
-import {styles} from './styles';
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from "../../assets/illustration.png";
+import { styles } from "./styles";
 
 export function SignIn() {
-    const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-    return (
-      <View style={styles.container}>
-          <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-          <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
 
-          <View style={styles.content}>
-            <Text style={styles.title}>Organize {`\n`} suas jogatinas {`\n`} facilmente</Text>
-            <Text style={styles.subTitle}>Crie grupos para jogar seus games {`\n`} favoritos com seus amigos</Text>
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {`\n`}e organize suas{`\n`} jogatinas
+        </Text>
+        <Text style={styles.subTitle}>
+          Crie grupos para jogar seus games {`\n`} favoritos com seus amigos
+        </Text>
 
-            <ButtonIcon title="Entrar com Discord" activeOpacity={0.7}/>
-          </View>
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
       </View>
-    );
-  }
-  
+    </View>
+  );
+}
